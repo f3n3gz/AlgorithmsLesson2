b9f1d33feb8f4cdd2deb8497195e940f8edd0c4b
fenegz <f3n3gz@gmail.com>
2019-02-16 13:11:13 +0500

    initial commit

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainApp {
     public static void main(String[] args) {
-        
+        final int BOUND=1_000_000;
+        int[] array=new int[BOUND];
+        Random random=new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i]=random.nextInt(BOUND);
+        }
     }
 }
