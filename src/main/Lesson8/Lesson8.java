5e11d9186fe84e1334720b49271f492979fbeb06
fenegz <f3n3gz@gmail.com>
2019-03-12 13:03:19 +0500

    Lesson8 initial commin

--- src/main/Lesson8/Lesson8.java
+++ src/main/Lesson8/Lesson8.java
@@ -11,10 +11,12 @@ public class Lesson8 {
 
         System.out.println(hashTable.get("Раз"));
         hashTable.put("Раз", 11);
-        System.out.println(hashTable.get("Раз"));
-        System.out.println(hashTable.get("Три"));
-        System.out.println(hashTable.get("Пять"));
-        System.out.println(hashTable.remove("Четыре"));
+        System.out.println("Раз " + hashTable.get("Раз"));
+        System.out.println("Три " + hashTable.get("Три"));
+        System.out.println("Пять " + hashTable.get("Пять"));
+        System.out.println("Size " + hashTable.getSize());
+        System.out.println("Значение удаленного элмента с ключом Четыре = " + hashTable.remove("Четыре"));
+        System.out.println("Size " + hashTable.getSize());
         hashTable.display();
     }
 }
